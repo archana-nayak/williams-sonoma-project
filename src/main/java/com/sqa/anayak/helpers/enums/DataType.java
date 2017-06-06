9136a6b0150e1ebd2f50a5226b147c0083df3e71
archananayak <archana.nayak@gmail.com>
2017-06-05 21:13:05 -0700

    Add enums folder to src/main/java

---
+++ src/main/java/com/sqa/anayak/helpers/enums/DataType.java
@@ -0,0 +1,5 @@
+package com.sqa.anayak.helpers.enums;
+
+public enum DataType {
+	BOOLEAN, CHAR, DOUBLE, FLOAT, INT, STRING
+}
---
+++ src/main/java/com/sqa/anayak/helpers/enums/TextFormat.java
@@ -0,0 +1,5 @@
+package com.sqa.anayak.helpers.enums;
+
+public enum TextFormat {
+	CSV, JSON, TAB, XML
+}
